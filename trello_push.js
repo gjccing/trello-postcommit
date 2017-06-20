df7eb19273675488a22b9311524666d24811fd5b
HGW <j2160204@gmail.com>
2017-06-21 00:27:24 +0800

    fix trello comment format

--- trello_push.js
+++ trello_push.js
@@ -19,25 +19,25 @@ Promise.all([
     return url.replace(/\.git$/, url.indexOf('bitbucket') > -1 ? '/commits/' : '/commit/');
   }),
   new Promise((res, rej)=>exec(
-    `(cd ${process.argv[2]} && git log --pretty=format:"%H %s" ` + 
+    `(cd ${process.argv[2]} && git log --pretty=format:"%H %B----------" ` + 
     (process.argv[3] != '0000000000000000000000000000000000000000'? process.argv[3] + '..' : '') +
     `${process.argv[4]})`,
     (error, stdout)=>error ? rej(error) : res(stdout.trim())
   ))
 ]).then(([url, commits])=>{
   commits
-    .split('\n')
+    .split('----------')
     .reverse()
     .filter(item=>item.search(/\|\s+https\:\/\/trello.com\/c\/\w+\/?\s*$/) > -1)
     .map(item=>item.trim())
     .map(item=>
       [item.substr(0, 40)]
-        .concat(item.substr(41).match(/(.+)\|\s+https\:\/\/trello.com\/c\/(\w+)\/?\s*$/).slice(1))
+        .concat(item.substr(41).match(/((.|\s)+)\|\s+https\:\/\/trello.com\/c\/(\w+)\/?\s*$/))
     )
     .reduce(
       (p, item)=>p.then(()=>new Promise((res, rej)=>t.post(
-        `/1/cards/${item[2]}/actions/comments/`,
-        { text: `Repository: ${repository}\nCommit: ${item[1].trim()}\nReference: ${url}${item[0]}` },
+        `/1/cards/${item[4]}/actions/comments/`,
+        { text: `Repository: ${repository}\nCommit:\n\n${item[2].trim()}\n\nReference: ${url}${item[0]}` },
         (error, data)=>error ? rej(error) : res(data)
       ))),
       Promise.resolve()
